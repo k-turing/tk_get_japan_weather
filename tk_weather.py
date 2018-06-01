06ac2f5983518f944369219b822df0abeed9427b
Kotrao Ito <turingsaketi@gmail.com>
2018-06-01 13:40:32 +0900

    plus print("\n")
    see clealy

--- tk_weather.py
+++ tk_weather.py
@@ -9,282 +9,329 @@ def push1():
     print("北海道の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
     
 def push2():
     weather_data = requests.get(api_url, params = {"city" : "020010"}).json() #get city name
     print("青森県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push3():
     weather_data = requests.get(api_url, params = {"city" : "020010"}).json() #get city name
     print("岩手県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
-            
+    print("\n")
+
 def push4():
     weather_data = requests.get(api_url, params = {"city" : "030010"}).json() #get city name
     print("宮城県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")
             
 def push5():
     weather_data = requests.get(api_url, params = {"city" : "040010"}).json() #get city name
     print("秋田県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push6():
     weather_data = requests.get(api_url, params = {"city" : "050010"}).json() #get city name
     print("山形県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
                     
 def push7():
     weather_data = requests.get(api_url, params = {"city" : "060010"}).json() #get city name
     print("福島県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
                 
 def push8():
     weather_data = requests.get(api_url, params = {"city" : "070010"}).json() #get city name
     print("茨城県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push9():
     weather_data = requests.get(api_url, params = {"city" : "080010"}).json() #get city name
     print("栃木県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push10():
     weather_data = requests.get(api_url, params = {"city" : "090010"}).json() #get city name
     print("群馬県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
             
 def push11():
     weather_data = requests.get(api_url, params = {"city" : "100010"}).json() #get city name
     print("埼玉県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
             
 def push12():
     weather_data = requests.get(api_url, params = {"city" : "110010"}).json() #get city name
     print("千葉県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push13():
     weather_data = requests.get(api_url, params = {"city" : "120010"}).json() #get city name
     print("東京都の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push14():
     weather_data = requests.get(api_url, params = {"city" : "130010"}).json() #get city name
     print("神奈川県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
             
 def push15():
     weather_data = requests.get(api_url, params = {"city" : "140010"}).json() #get city name
     print("新潟県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push16():
     weather_data = requests.get(api_url, params = {"city" : "150010"}).json() #get city name
     print("富山県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push17():
     weather_data = requests.get(api_url, params = {"city" : "160010"}).json() #get city name
     print("石川県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push18():
     weather_data = requests.get(api_url, params = {"city" : "170010"}).json() #get city name
     print("福井県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push19():
     weather_data = requests.get(api_url, params = {"city" : "180010"}).json() #get city name
     print("山梨県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push20():
     weather_data = requests.get(api_url, params = {"city" : "190010"}).json() #get city name
     print("長野県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push21():
     weather_data = requests.get(api_url, params = {"city" : "200010"}).json() #get city name
     print("岐阜県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push22():
     weather_data = requests.get(api_url, params = {"city" : "210010"}).json() #get city name
     print("静岡県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push23():
     weather_data = requests.get(api_url, params = {"city" : "220010"}).json() #get city name
     print("愛知県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push24():
     weather_data = requests.get(api_url, params = {"city" : "230010"}).json() #get city name
     print("三重県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push25():
     weather_data = requests.get(api_url, params = {"city" : "240010"}).json() #get city name
     print("滋賀県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push26():
     weather_data = requests.get(api_url, params = {"city" : "250010"}).json() #get city name
     print("京都府の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push27():
     weather_data = requests.get(api_url, params = {"city" : "260010"}).json() #get city name
     print("大阪府の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push28():
     weather_data = requests.get(api_url, params = {"city" : "270010"}).json() #get city name
     print("兵庫県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push29():
     weather_data = requests.get(api_url, params = {"city" : "280010"}).json() #get city name
     print("奈良県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push30():
     weather_data = requests.get(api_url, params = {"city" : "290010"}).json() #get city name
     print("和歌山県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push31():
     weather_data = requests.get(api_url, params = {"city" : "300010"}).json() #get city name
     print("鳥取県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push32():
     weather_data = requests.get(api_url, params = {"city" : "310010"}).json() #get city name
     print("島根県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push33():
     weather_data = requests.get(api_url, params = {"city" : "320010"}).json() #get city name
     print("岡山県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push34():
     weather_data = requests.get(api_url, params = {"city" : "330010"}).json() #get city name
     print("広島県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push35():
     weather_data = requests.get(api_url, params = {"city" : "340010"}).json() #get city name
     print("山口県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push36():
     weather_data = requests.get(api_url, params = {"city" : "350010"}).json() #get city name
     print("徳島県の")
     for i in weather_data["forecasts"]: 
-        print(i["dateLabel"] + "の天気は、" + i["telop"])        
+        print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")            
 
 def push37():
     weather_data = requests.get(api_url, params = {"city" : "360010"}).json() #get city name
     print("香川県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push38():
     weather_data = requests.get(api_url, params = {"city" : "370010"}).json() #get city name
     print("愛媛県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push39():
     weather_data = requests.get(api_url, params = {"city" : "380010"}).json() #get city name
     print("高知県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push40():
     weather_data = requests.get(api_url, params = {"city" : "390010"}).json() #get city name
     print("福岡県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push41():
     weather_data = requests.get(api_url, params = {"city" : "400010"}).json() #get city name
     print("佐賀県の")
     for i in weather_data["forecasts"]: 
-        print(i["dateLabel"] + "の天気は、" + i["telop"])        
+        print(i["dateLabel"] + "の天気は、" + i["telop"])  
+    print("\n")          
 
 def push42():
     weather_data = requests.get(api_url, params = {"city" : "410010"}).json() #get city name
     print("長崎県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push43():
     weather_data = requests.get(api_url, params = {"city" : "420010"}).json() #get city name
     print("熊本県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push44():
     weather_data = requests.get(api_url, params = {"city" : "430010"}).json() #get city name
     print("大分県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push45():
     weather_data = requests.get(api_url, params = {"city" : "450010"}).json() #get city name
     print("宮崎県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push46():
     weather_data = requests.get(api_url, params = {"city" : "460010"}).json() #get city name
     print("鹿児島県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 def push47():
     weather_data = requests.get(api_url, params = {"city" : "471010"}).json() #get city name
     print("沖縄県の")
     for i in weather_data["forecasts"]: 
         print(i["dateLabel"] + "の天気は、" + i["telop"])
+    print("\n")    
 
 #variable
 
